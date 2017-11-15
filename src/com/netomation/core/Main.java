67052c51c9f3656d2f1c1df622f4c1cd81448d6a
tchvu3 <tchvu3@gmail.com>
2017-11-15 11:41:38 +0200

    Updated the programs design pattern to: 'Factory Method Design Pattern'

--- src/com/netomation/core/Main.java
+++ src/com/netomation/core/Main.java
@@ -9,5 +9,17 @@ public class Main {
 
     public static void main(String[] args) {
 
+
+        // Dynamic creation, can be at run time!
+        SocialNetwork facebook1 = SocialNetworkFactory.getSocialNetwork("facebook");
+        SocialNetwork twitter1 = SocialNetworkFactory.getSocialNetwork("twitter");
+
+        // Static creation
+        SocialNetwork facebook2 = SocialNetworkFactory.getSocialNetwork(FacebookWrapper.class);
+        SocialNetwork twitter2 = SocialNetworkFactory.getSocialNetwork(TwitterWrapper.class);
+
+        // The factory class keep the latest social network instantiated
+        SocialNetwork latest = SocialNetworkFactory.getLatest(); // latest equals now to "twitter2"
+
     }
 }
